4a36d91f3ff96cc92d4ea1ed356162c387586678
yukihira1992 <hirayama@cancerscan.jp>
2019-07-09 18:44:39 +0900

    Removed .env files and defined environment variables in docker-compose.yml

--- src/config/settings.py
+++ src/config/settings.py
@@ -16,7 +16,6 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -28,10 +27,11 @@ DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
 TEST = 'test' in sys.argv
 RELEASE_STAGE = os.environ.get('RELEASE_STAGE', 'local').lower()
 
-if RELEASE_STAGE == 'local':
-    ALLOWED_HOSTS = ['*']
-else:
-    ALLOWED_HOSTS = ['.example.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.example.com',
+]
 
 # Application definition
 
@@ -76,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 if TEST:
@@ -119,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -132,7 +130,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
